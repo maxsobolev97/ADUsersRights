953ec7f355fe5f345940524b72c9fc350046fce2
maxsobolev97 <77441161+maxsobolev97@users.noreply.github.com>
2022-03-24 13:42:55 +0300

    Add files via upload

---
+++ Users.ps1
@@ -0,0 +1,29 @@
+﻿$a = Get-ADUser -Identity *
+$file = "C:\example\ADCSV.csv"
+
+foreach($b in $a){
+
+    if($b.Enabled){
+    
+       $c = Get-ADPrincipalGroupMembership $b | Sort-Object | select -ExpandProperty name
+       foreach($d in $c){
+        $z = Get-ADGroup $d -Properties * | Select Description
+        if($z.Description -ne $null){
+
+            [string]$toCSV = $b.Name + "," + $d + "," + $z.Description
+
+        } else {
+
+            [string]$toCSV = $b.Name + "," + $d
+
+        }
+        Write-Output $toCSV >> $file
+       
+       }
+
+    }
+    
+
+}
+
+Write-Output "All Done!"
